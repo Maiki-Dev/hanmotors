331c1468340ea5e1f6eaeb6649ff8d67c9c83d90
Ankhbayar Zoltuya <nozum1ke@gmail.com>
2026-02-05 13:33:17 +0800

    Config: Enable HTTPS in Nginx and update app configs

--- customer-app/src/config.ts
+++ customer-app/src/config.ts
@@ -1,5 +1,5 @@
 // Replace this with your computer's current local IP address
-export const API_URL = 'http://khanmotors.cloud'; // VPS Server Domain
+export const API_URL = 'https://khanmotors.cloud'; // VPS Server Domain (HTTPS)
 
 // Google Maps API Key
 export const GOOGLE_MAPS_APIKEY = process.env.EXPO_PUBLIC_GOOGLE_MAPS_APIKEY || 'AIzaSyDYtKufZG0X6JAS4lpEV8ZtfenGLvBf2Es';
--- driver-app/src/config.js
+++ driver-app/src/config.js
@@ -1,7 +1,7 @@
 // Replace this with your computer's current local IP address
 // Example: 'http://192.168.1.5:5000'
 // // export const API_URL = 'http://192.168.1.16:5000'; // Real Device
-export const API_URL = 'http://khanmotors.cloud'; // VPS Server Domain
+export const API_URL = 'https://khanmotors.cloud'; // VPS Server Domain (HTTPS)
 
 // Google Maps API Key - Replace with your valid API key
 export const GOOGLE_MAPS_APIKEY = process.env.EXPO_PUBLIC_GOOGLE_MAPS_APIKEY || 'AIzaSyDYtKufZG0X6JAS4lpEV8ZtfenGLvBf2Es';
